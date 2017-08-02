37eaf24e191b6845c747b343ae1620ff34ee4413
yatchan <jazzguitar.tokyo@gmail.com>
2017-08-02 11:00:00 +0000

    dive11 contact mail

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,9 +1,21 @@
 class NoticeMailer < ApplicationMailer
-  # 引数としてblogを追加します。
-  def sendmail_blog(blog)
-    @blog = blog
 
-    mail to: "dive154@dc1.test154.etius.jp"
-         subject:'【Achieve】ブログが投稿されました'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_blog.subject
+  #
+  def sendmail_blog(blog)
+   @blog = blog
+
+   mail to: "dive154@dc1.test154.etius.jp",
+    subject: '【Achieve】ブログが投稿されました'
+  end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+   mail to: @contact.email,
+   subject: '【Achieve】コンタクトが投稿されました'
   end
 end
