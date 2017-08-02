0505c467baccc4596ddf997c599fdea0049332bb
yatchan <jazzguitar.tokyo@gmail.com>
2017-08-02 06:26:32 +0000

    dive11 ブログ投稿時のエラー修正

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,14 +1,9 @@
 class NoticeMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notice_mailer.sendmail_blog.subject
-  #
+  # 引数としてblogを追加します。
   def sendmail_blog(blog)
     @blog = blog
 
     mail to: "abcdefg@example.com"
-    subject:'【Achieve】ブログが投稿されました'
+         subject:'【Achieve】ブログが投稿されました'
   end
 end
