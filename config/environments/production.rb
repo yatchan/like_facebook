8e8371117fa3a4de246b9ecce20987f5ca7c2781
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-11 10:30:06 +0000

    host aws 変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'enigmatic-bayou-49063.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '13.115.46.249' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
